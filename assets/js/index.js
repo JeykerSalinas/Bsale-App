8dd8924ad30d35304fbafadd22cf0902f0ef7a7c
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-04-06 17:54:34 -0400

    Making style changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,22 @@ const getData = async () => {
   try {
     const request = await fetch(PRODUCTS_URL);
     const data = await request.json();
+    document.getElementById("myData").innerHTML = "";
     data.map((product) => {
       document.getElementById(
         "myData"
-      ).innerHTML += `<div class="card col-12 col-md-6 col-lg-4 m-4 rounded" style="width: 18rem;">
-      <img src="${
+      ).innerHTML += `<div class="my-card card col-12 col-md-6 col-lg-4 m-4 rounded" style="width: 18rem;">
+<div>
+<img src="${
         product.url_image
           ? product.url_image
           : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
-      }" class="card-img-top" alt="${product.name}">
+      }" class="card-img-top " alt="${product.name}">
+
+</div>
       <div class="card-body">
         <p class="card-text">${product.name}</p>
-        <p class="card-text">$${product.price.toLocaleString("de-DE")}</p>
+        <p class="card-text price">$${product.price.toLocaleString("de-DE")}</p>
       </div>
     </div>`;
     });
@@ -29,11 +33,13 @@ const getCategory = async () => {
   try {
     const request = await fetch(CATEGORY_URL);
     const data = await request.json();
-    data.map((category) => {
-      document.getElementById(
-        "categories"
-      ).innerHTML += `<li class="list-group-item" id="${category.id}" onclick="getProductoCategory(id)">${category.name}</li>`;
-    });
+
+    data.map(
+      (a) =>
+        (document.getElementById(
+          "categories"
+        ).innerHTML += `<li><a class="dropdown-item" href="#">${a.name}</a></li>`)
+    );
     console.log(data);
   } catch (error) {
     console.error(error);
@@ -45,7 +51,21 @@ const filterByName = async (search) => {
   try {
     const request = await fetch(PRODUCTS_BY_NAME);
     const data = await request.json();
-    console.log(data);
+    document.getElementById("myData").innerHTML = data.map(
+      (
+        product
+      ) => `<div class="card col-12 col-md-6 col-lg-4 m-4 rounded" style="width: 18rem;">
+    <img src="${
+      product.url_image
+        ? product.url_image
+        : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
+    }" class="card-img-top" alt="${product.name}">
+    <div class="card-body">
+      <p class="card-text">${product.name}</p>
+      <p class="card-text">$${product.price.toLocaleString("de-DE")}</p>
+    </div>
+  </div>`
+    );
   } catch (error) {
     console.error(error);
   }
