8e22ddb9de7d46194c2b07c961c30a4463ca8b90
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-04-07 16:39:00 -0400

    Refactoring .innerHtml

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,41 @@
-const getData = async () => {
+const cardsLayout = (data) => {
+  const cardsNode = document.getElementById("myData");
+  data.map((product) => {
+    cardsNode.innerHTML += `<div class="my-card card col-12 col-md-6 col-lg-4 m-4  rounded" style="width: 18rem;">
+    <div class="h-100 d-flex align-items-center border-bottom">
+    <img src="${
+      product.url_image
+        ? product.url_image
+        : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
+    }" class="card-img-top img-fluid  " alt="${product.name}">
+    
+    </div>
+          <div class="card-body">
+            <ul class="list-group list-group-flush">
+          <li class="list-group-item ">
+          ${product.name}</li>
+          <li class="list-group-item price text-end">$${product.price.toLocaleString(
+            "de-DE"
+          )}</li>
+        </ul>
+          </div>
+        </div>`;
+  });
+};
+
+const getData = (async () => {
   const PRODUCTS_URL = "http://localhost:3600/bsale/products";
   try {
     const request = await fetch(PRODUCTS_URL);
     const data = await request.json();
     document.getElementById("myData").innerHTML = "";
-    data.map((product) => {
-      document.getElementById(
-        "myData"
-      ).innerHTML += `<div class="my-card card col-12 col-md-6 col-lg-4 m-4  rounded" style="width: 18rem;">
-<div class="h-100 d-flex align-items-center border-bottom">
-<img src="${
-        product.url_image
-          ? product.url_image
-          : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
-      }" class="card-img-top img-fluid  " alt="${product.name}">
-
-</div>
-      <div class="card-body">
-        <ul class="list-group list-group-flush">
-      <li class="list-group-item ">
-      ${product.name}</li>
-      <li class="list-group-item price text-end">$${product.price.toLocaleString(
-        "de-DE"
-      )}</li>
-    </ul>
-      </div>
-    </div>`;
-    });
-    return "Hola"
+    cardsLayout(data);
   } catch (error) {
     console.log(error);
   }
-};
+})();
 
-const getCategory = async () => {
+const getCategory = (async () => {
   const CATEGORY_URL = "http://localhost:3600/bsale/category";
   try {
     const request = await fetch(CATEGORY_URL);
@@ -48,43 +50,23 @@ const getCategory = async () => {
   } catch (error) {
     console.error(error);
   }
-};
+})();
 
 const filterByName = async (search) => {
   const PRODUCTS_BY_NAME = `http://localhost:3600/bsale/products/name/${search}`;
   try {
     const request = await fetch(PRODUCTS_BY_NAME);
     const data = await request.json();
-    console.log(data)
-    document.getElementById(
-      "myData"
-    ).innerHTML=""
-    data.map(
-      (product) =>
-        (document.getElementById(
-          "myData"
-        ).innerHTML += `<div class="card col-12 col-md-6 col-lg-4 m-4 rounded" style="width: 18rem;">
-    <img src="${
-      product.url_image
-        ? product.url_image
-        : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
-    }" class="card-img-top" alt="${product.name}">
-    <div class="card-body">
-      <p class="card-text">${product.name}</p>
-      <p class="card-text">$${product.price.toLocaleString("de-DE")}</p>
-    </div>
-  </div>`)
-    );
+    console.log(data);
+    document.getElementById("myData").innerHTML = "";
+    cardsLayout(data);
   } catch (error) {
-    document.getElementById(
-      "myData"
-    ).innerHTML = '<p>Lo sentimos, no se encontraron resultados...</p>'
+    document.getElementById("myData").innerHTML =
+      "<p>Lo sentimos, no se encontraron resultados...</p>";
     console.error(error);
   }
 };
 
-getData();
-getCategory();
 const submitButton = document.getElementById("submitButton");
 const myForm = document.getElementById("myForm");
 const search = document.getElementById("mySearch");
@@ -98,23 +80,7 @@ const filterByCategory = async (search) => {
   try {
     const request = await fetch(PRODUCTS_BY_CATEGORY);
     const data = await request.json();
-    console.log(data);
-    data.map(
-      (product) =>
-        (document.getElementById(
-          "myData"
-        ).innerHTML += `<div class="card col-12 col-md-6 col-lg-4 m-4 rounded" style="width: 18rem;">
-    <img src="${
-      product.url_image
-        ? product.url_image
-        : "https://www.bevi.com/static/files/0/ecommerce-default-product.png"
-    }" class="card-img-top" alt="${product.name}">
-    <div class="card-body">
-      <p class="card-text">${product.name}</p>
-      <p class="card-text">$${product.price.toLocaleString("de-DE")}</p>
-    </div>
-  </div>`)
-    );
+    cardsLayout(data);
   } catch (error) {
     console.error(error);
   }
@@ -125,4 +91,4 @@ const filterCardsByCategory = (id) => {
   id === 0 ? getData() : filterByCategory(id);
 };
 
-console.log(getData())
+
