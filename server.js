7a21f5662791a137f4462fb59cc2b4b3addac589
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-04-08 12:05:33 -0400

    Cleaning repository

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(__dirname));
-const port = process.env.PORT || 3000;
-app.listen(port);
